476e39a40921e99930bb930757c3fc672ed514b2
Glenn-Rhee <glennviktor5@gmail.com>
2026-01-11 22:17:45 +0700

    feat: setup express

---
+++ src/app/web.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const PORT = 8001 as const;
+
+app.use(
+  cors({
+    origin: ["http:/localhost:3000"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/", (req, res) => {
+  res.json("cihuyyy");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT);
+});
