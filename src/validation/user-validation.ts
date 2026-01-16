d8fe288f00460956b9faa5ec6b868cd604db68c2
Glenn-Rhee <glennviktor5@gmail.com>
2026-01-16 21:50:22 +0700

    refactor: add editSchema

--- src/validation/user-validation.ts
+++ src/validation/user-validation.ts
@@ -8,4 +8,12 @@ export default class UserValidation {
       .min(1, { error: "Minimum length of username is 1" }),
     imageUrl: z.url({ error: "Please fill image url properly!" }),
   });
+
+  static readonly EDITSCHEMA = z.object({
+    username: z.string({ error: "Please fill username properly!" }),
+    gender: z.enum(["UNKNOWN", "MALE", "FEMALE"], {
+      error: "Please fill gender just between Unknwon, Male, and Female",
+    }),
+    birthday: z.date({ error: "Please fill birthday properly!" }).optional(),
+  });
 }
